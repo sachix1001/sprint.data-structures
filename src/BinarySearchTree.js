2dfc33943c857194bcc0e18f519281e377ea11a1
Sachi Grannan <sachix1001@gmail.com>
2020-01-10 08:57:41 +0900

    working on BinaryTree traverse

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -8,15 +8,62 @@ class BinarySearchTree {
   insert(val) {
     //if it is less than BinarySearchTree(value) then:
     let inserted = new BinarySearchTree(val);
-    if (val < this.value) {
-      this.left = inserted;
-      // console.log(inserted);
+    if (val === this.value) {
       return this;
     }
-    //if it is more
-    this.right = inserted;
-    // console.log(inserted);
+    if (val < this.value) {
+      if (!this.left) {
+        this.left = inserted;
+        return this;
+      }
+      this.left.insert(val);
+    }
+    if (!this.right) {
+      this.right = inserted;
+      return this;
+    } else {
+      this.right.insert(val);
+    }
     return this;
+  }
+
+  contains(value) {
+    if (value === this.value) {
+      return true;
+    }
+    if (value < this.value && this.left) {
+      return this.left.contains(value);
+    }
+    if (value > this.value && this.right) {
+      return this.right.contains(value);
+    }
+    return false;
+  }
+
+  // traverseDepthFirstInOrder(callback) {
+  //   function recurse(node) {
+  //     if (node.left) {
+  //       recurse(node.left);
+  //     }
+  //     callback(node);
+  //     if (node.right) {
+  //       recurse(node.right);
+  //     }
+  //   }
+  //   recurse(this);
+  // }
+
+  traverseDepthFirstInOrder(callback) {
+    function traverse(node) {
+      if (node.left) {
+        traverse(node.left);
+      }
+      callback(node);
+      if (node.right) {
+        traverse(node.right);
+      }
+    }
+    traverse(this);
   }
 }
 module.exports = { BinarySearchTree: BinarySearchTree };
